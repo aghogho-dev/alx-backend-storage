b0de8ad9ff3f73412f23f0328e18cef8de378cd5
Aghogho <monorienaghogho@gmail.com>
2023-07-27 17:40:22 +0000

    11. meeting

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,8 @@
+-- 11. TABLE
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS (
+	SELECT name FROM students
+	WHERE score < 80 AND (
+		last_meeting IS NULL OR last_meeting < ADDDATE(CURDATE(), interval -1 MONTH)
+	);
+)
