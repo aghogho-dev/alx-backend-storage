e8a866b029df2ce0fa7f8f539156668cb30aac3f
Aghogho <monorienaghogho@gmail.com>
2023-07-27 17:35:51 +0000

    10, safe div

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -4,10 +4,12 @@ DELIMITER //
 CREATE FUNCTION SafeDiv(a INTEGER, b INTEGER)
 RETURNS FLOAT DETERMINISTIC
 BEGIN
-	IF (b == 0)
-	THEN RETURN (0);
-	ELSE RETURN (a / b);
-	END IF;
+    IF (b = 0)
+    THEN
+        RETURN (0);
+    ELSE
+        RETURN (a / b);
+    END IF;
 END
 //
 DELIMITER ;
